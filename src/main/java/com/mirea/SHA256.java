72336aad9aa27563561f533baeda7b684a21ffbb
O33ero <58762519+O33ero@users.noreply.github.com>
2021-11-06 13:36:34 +0300

    Polishing #1 and Fixing error with 448 bits array. Now SHA256 working fully correct.

--- src/main/java/com/mirea/SHA256.java
+++ src/main/java/com/mirea/SHA256.java
@@ -2,11 +2,14 @@ package com.mirea;
 
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
+import java.security.InvalidParameterException;
 
 public class SHA256 {
 
     
-
+    /**
+     * Константы
+     */
     private static int[] k = {
         0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
         0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
@@ -18,18 +21,38 @@ public class SHA256 {
         0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2 
     };
 
+    /**
+     * Устанавливает {@code c}-ый бит числа {@code t} в значение {@code v}
+     * @param t - число
+     * @param c - номер бита
+     * @param v - значение бита
+     * @return Число с изменненым битом
+     * 
+     * @see https://stackoverflow.com/questions/4674006/set-specific-bit-in-byte
+     */
+    private static int setBit(int t, int c, int v) { 
+        if (v != 1 && v != 0) 
+            throw new InvalidParameterException("New value of bit should be 0 or 1");
+
+        if (v == 1) 
+            t |= 1 << c;
+        else 
+            t &= ~(1 << c);
+
+        return t;
+    }
 
     /**
      * Переводит из массива байтов в массив битов
-     * @param bitContainer - массив битов, в который будет сделана запись
+     * @param bitArray - массив битов, в который будет сделана запись
      * @param source - источник байтов
      * @return Последний пустой элемент
      */
-    private static int convertToBits(byte[] bitContainer, byte[] source) {
+    private static int convertToBits(byte[] bitArray, byte[] source) {
         for(int i = 0; i < source.length * 8; i++) {
             byte nowByte = source[i / 8];
 
-            bitContainer[i] = (byte)(nowByte >> (7 - (i % 8)) & 1);
+            bitArray[i] = (byte)(nowByte >> (7 - (i % 8)) & 1);
         }
 
         return source.length * 8;
@@ -37,38 +60,38 @@ public class SHA256 {
 
 
     /**
-     * Запись размера контента в послднии 64 байта {@code bitConteiner} 
-     * @param bitContainer - массив битов
+     * Запись размера контента в послднии 64 байта {@code bitArray} 
+     * @param bitArray - массив битов
      * @param contentSize - размер контента
      */
-    private static void writeContentSize(byte[] bitContainer, long contentSize) {
+    private static void writeContentSize(byte[] bitArray, long contentSize) {
         int rnd = 63;
-        for(int i = bitContainer.length - 64; i < bitContainer.length; i++) {
-            bitContainer[i] = (byte)(contentSize >> rnd & 1);
+        for(int i = bitArray.length - 64; i < bitArray.length; i++) {
+            bitArray[i] = (byte)(contentSize >> rnd & 1);
             rnd--;
         }
     }
 
 
     /**
-     * Запись из массива битов {@code bitContainer} в массив слов (слово = 32 байта) {@code words}. 
-     * @param bitContainer - массив битов
-     * @param words - массив слов
+     * Запись из массива битов {@code bitArray} в массив слов (слово = 32 бита) {@code w}. 
+     * @param bitArray - массив битов
+     * @param w - массив слов
      * @param count - количество слов, которые надо скопировать
      */
-    private static void writeWords(byte[] bitContainer, int[] words, int count) {
-        int j = count - 1;   // Счетчит слов
+    private static void writeWords(byte[] bitArray, int[] w, int count) {
+        int j = count - 1;
         int rnd = 0;
         int temp = 0;
         for(int i = count * 32 - 1; i >= 0 ; i--) {
 
-            if(bitContainer[i] == 1) {
-                temp |= 1 << rnd; // Устанавливает rnd бит в 1
+            if(bitArray[i] == 1) {
+                temp = setBit(temp, rnd, 1);
             }
             rnd++;
             
             if (i % 32 == 0) {
-                words[j] = temp;
+                w[j] = temp;
                 j--;
                 temp = 0;
                 rnd = 0;
@@ -89,11 +112,11 @@ public class SHA256 {
         int result = src;
         for(int i = 0; i < count; i++) {
             int bit = src & 1; // Последний бит
-            src = src >> 1;
-            if (bit == 1) // https://stackoverflow.com/questions/4674006/set-specific-bit-in-byte
-                src |= 1 << 31;
+            src = src >>> 1;
+            if (bit == 1) 
+                src = setBit(src, 31, 1);   
             else
-                src &= ~(1 << 31);
+                src = setBit(src, 31, 0);
 
             result = src;
 
@@ -112,12 +135,14 @@ public class SHA256 {
         for(int i = 0; i < count; i++) {
             src = src >> 1;
 
-            src &= ~(1 << 31);// https://stackoverflow.com/questions/4674006/set-specific-bit-in-byte
+            src = setBit(src, 31, 0);
 
             result = src;
         }
 
         return result;
+
+        // return src >>> count;
     }
 
     /**
@@ -125,7 +150,7 @@ public class SHA256 {
      * @param source - исходный массив слов
      * @return Массив байт
      */
-    public static byte[] convertToBytes(int[] source) {
+    private static byte[] convertToBytes(int[] source) {
         byte[] result = new byte[source.length * 4];
         int index = 0;
 
@@ -143,8 +168,9 @@ public class SHA256 {
      * Превращает массив байтов в 16-ричною строку
      * @param bytes - массив байт
      * @return Строка в 16-ричном виде
+     * @see https://stackoverflow.com/questions/9655181/how-to-convert-a-byte-array-to-a-hex-string-in-java
      */
-    public static String bytesToHex(byte[] bytes) { // https://stackoverflow.com/questions/9655181/how-to-convert-a-byte-array-to-a-hex-string-in-java
+    public static String bytesToHex(byte[] bytes) {
         byte[] HEX_ARRAY = "0123456789abcdef".getBytes(StandardCharsets.US_ASCII);
         byte[] hexChars = new byte[bytes.length * 2];
         for (int j = 0; j < bytes.length; j++) {
@@ -159,8 +185,9 @@ public class SHA256 {
      * Высчитываем хеш-сумму по алгоритму {@code SHA-256} для массива битов {@code content}
      * @param content - массив битов
      * @return Хеш-сумма
+     * @see https://tproger.ru/translations/sha-2-step-by-step/
      */
-    public static byte[] getHash(byte[] content) { // https://tproger.ru/translations/sha-2-step-by-step/
+    public static byte[] getHash(byte[] content) {
 
         /**
          * Начальные параметры
@@ -173,53 +200,52 @@ public class SHA256 {
         int h5 = 0x9b05688c; 
         int h6 = 0x1f83d9ab; 
         int h7 = 0x5be0cd19; 
+
         /**
          * Считаем размер
          */
-        int bitContainerSize = content.length * 8 ;                             // Чисто контент
-        bitContainerSize += 1;                                                  // 1 бит равный единице
-        bitContainerSize = bitContainerSize + (512 - bitContainerSize % 512);   // Выравниваем до 512; 
+        int bitArraySize = content.length * 8 ;                             // Чисто контент
+        bitArraySize += 1;                                                  // 1 бит равный единице
+        bitArraySize += 64;                                                 // Оставляем место под размер содержимого
+        bitArraySize = bitArraySize + (512 - bitArraySize % 512);           // Выравниваем до 512; 
         
 
         /**
          * Предворительное заполнение
          */
-        byte[] bitContainer = new byte[bitContainerSize];                       // Инициализация массива бит
-        int lastIndex = convertToBits(bitContainer, content);                   // Чисто контент
-        bitContainer[lastIndex] = 1;                                            // Приколюхная единичка
-        long contentSize = content.length * 8;                                  // Размер контента
-        writeContentSize(bitContainer, contentSize);
+        byte[] bitArray = new byte[bitArraySize];                           // Инициализация массива бит
+        int lastIndex = convertToBits(bitArray, content);                   // Чисто контент
+        bitArray[lastIndex] = 1;                                            // Приколюхная единичка
+        long contentSize = content.length * 8;                              // Размер контента
+        writeContentSize(bitArray, contentSize);
 
 
 
         /**
          * Основной цикл хеширования
          */
-        int[] hArr = new int [8];
 
-        for(int i = 0; i < bitContainerSize / 512; i++) { 
-            byte[] subBitContainer = new byte[512];
-            System.arraycopy(bitContainer, i * 512, subBitContainer, 0, 512);   // Для каждого 512-битного куска
+        for(int i = 0; i < bitArraySize / 512; i++) { 
+            byte[] subBitArray = new byte[512];                             // Берем кусок битового массива длиной 512 бит
+            System.arraycopy(bitArray, i * 512, subBitArray, 0, 512);       
 
-            int[] words = new int[64];                                          // Добавляем слов до 64, инициализированных нулями, 
-            writeWords(subBitContainer, words, 16); 
+            int[] w = new int[64];                                          // Добавляем 48 32-битных слов, заполненые нулями 
+            writeWords(subBitArray, w, 16);                                 // Записываем первые 16 слов (512 бит)
 
             for(int j = 16; j < 64; j++) {
-                int a = roundRotateRight(words[j - 15], 7);
-                int b = roundRotateRight(words[j - 15], 18);
-                int c = shiftRotateRight(words[j - 15], 3);
+                int a = roundRotateRight(w[j - 15], 7);
+                int b = roundRotateRight(w[j - 15], 18);
+                int c = shiftRotateRight(w[j - 15], 3);
 
                 int s0 = a ^ b ^ c;
 
-                a = roundRotateRight(words[j - 2], 17);
-                b = roundRotateRight(words[j - 2], 19);
-                c = shiftRotateRight(words[j - 2], 10);
+                a = roundRotateRight(w[j - 2], 17);
+                b = roundRotateRight(w[j - 2], 19);
+                c = shiftRotateRight(w[j - 2], 10);
 
                 int s1 = a ^ b ^ c;
 
-                a = words[j - 16];
-                b = words[j - 7];
-                words[j] = words[j - 16] + s0 + words[j - 7] + s1;
+                w[j] = w[j - 16] + s0 + w[j - 7] + s1;
             }
 
             /**
@@ -236,7 +262,7 @@ public class SHA256 {
             for(int j = 0; j < 64; j++) {
                 int S1 = roundRotateRight(e, 6) ^ roundRotateRight(e, 11) ^ roundRotateRight(e, 25);
                 int ch = (e & f) ^ ((~e) & g);
-                int temp1 = h + S1 + ch + k[j] + words[j];
+                int temp1 = h + S1 + ch + k[j] + w[j];
                 int S0 = roundRotateRight(a, 2) ^ roundRotateRight(a, 13) ^ roundRotateRight(a, 22);
                 int maj = (a & b) ^ (a & c) ^ (b & c);
                 int temp2 = S0 + maj;
@@ -261,9 +287,7 @@ public class SHA256 {
             h7 = h7 + h;
         }
 
-
-
-
+        int[] hArr = new int [8];
         hArr[0] = h0;
         hArr[1] = h1;
         hArr[2] = h2;
