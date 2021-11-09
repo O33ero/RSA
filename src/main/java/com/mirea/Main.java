48f2085380bff8a0fc9fae8b859fe0d819a2df76
O33ero <58762519+O33ero@users.noreply.github.com>
2021-11-09 16:44:02 +0300

    Added "user interface"

--- src/main/java/com/mirea/Main.java
+++ src/main/java/com/mirea/Main.java
@@ -1,11 +1,69 @@
 package com.mirea;
 
-import java.io.FileOutputStream;
-import java.math.BigInteger;
-import java.util.Random;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.NoSuchFileException;
+import java.security.NoSuchAlgorithmException;
+
+
 
 public class Main {
-    public static void main(String[] args) {
+    
+    static String info[] = {
+        "Functional:\n",
+        "    Generate key: java Sign.jar -k\n",
+        "    Encrypt file: java Sign.jar -e <PATH to file> <PATH to private key>\n",
+        "    Decrypt file: java Sign.jar -d <PATH to file> <PATH to public key>\n"
+    };
+
+    public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
         
+        String filename = "";
+        String privatekey = "";
+        String publickey = "";
+
+        if (args.length == 0) {
+            System.out.println("Cannot detect flag <-h, -k, -e, -d>");
+            for(String now : info) {
+                System.out.print(now);
+            }
+            return;
+        }
+
+        switch (args[0]) {
+            case "-h":
+                for(String now : info) {
+                    System.out.print(now);
+                }
+                break;
+            case "-k":
+                RSA.generateKeys();
+            case "-e":
+                filename = args[1];
+                privatekey = args[2];
+                try {
+                    RSA.signingFile(filename, privatekey);
+                }
+                catch (NoSuchFileException e) {
+                    System.out.println("ERROR: file " + filename + " is not exist");
+                }
+                break;
+            case "-d":
+                filename = args[1];
+                publickey = args[2];
+                try {
+                    RSA.checkSign(filename, publickey);
+                }
+                catch (NoSuchFileException e) {
+                    System.out.println("ERROR: file " + filename + " is not exist");
+                }
+                break;
+            default:
+                System.out.println("Cannot detect flag <-h, -k, -e, -d>");
+                for(String now : info) {
+                    System.out.print(now);
+                }
+                break;
+        }
     }
 }
